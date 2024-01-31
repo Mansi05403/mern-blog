4bb63d926989d398a4d9ce6e701329ef789e4633
Mansi Agarwal <mansiagar05@gmail.com>
2024-01-31 21:54:41 +0530

    add update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,13 +11,10 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
-import PostPage from './pages/PostPage';
-import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <ScrollToTop />
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
@@ -33,7 +30,6 @@ export default function App() {
         </Route>
 
         <Route path='/projects' element={<Projects />} />
-        <Route path='/post/:postSlug' element={<PostPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
